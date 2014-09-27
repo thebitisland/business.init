f2bd57b0bfeb47c79fbfd397074553387146139d
Juan Luis Sanz <juanljr2@gmail.com>
2014-09-27 16:28:54 +0200

    Added zoner

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,31 +1,293 @@
-<!doctype html>
-<html class="no-js" lang="">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
 
-        <!-- Place favicon.ico and apple-touch-icon(s) in the root directory -->
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="author" content="The bit Island">
 
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/main.css">
-<script src="js/vendor/modernizr-2.8.3.min.js"></script>
+	<link href='http://fonts.googleapis.com/css?family=Roboto:300,400,700' rel='stylesheet' type='text/css'>
+	<link href="assets/fonts/font-awesome.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/bootstrap-select.min.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/jquery.slider.min.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/osm.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/leaflet.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/MarkerCluster.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/style.css" type="text/css">
+
+	<title>{{ $title or 'Business.init()' }}</title>
+
 </head>
-<body>
-<!--[if lt IE 8]>
-<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+
+<body class="page-homepage navigation-fixed-bottom has-fullscreen-map map-osm" id="page-top" data-spy="scroll" data-target=".navigation" data-offset="90">
+	<!-- Wrapper -->
+	<div class="wrapper">
+
+		<div class="navigation">
+			<div class="secondary-navigation">
+				<div class="container">
+					<div class="user-area">
+						<a href="create-agency.html" class="promoted">Create Agency</a>
+						<a href="create-account.html" class="promoted"><strong>Register</strong></a>
+						<a href="sign-in.html">Sign In</a>
+						<div class="language-bar">
+							<a href="#" class="active"><img src="assets/img/flags/gb.png"></a>
+							<a href="#"><img src="assets/img/flags/de.png"></a>
+							<a href="#"><img src="assets/img/flags/es.png"></a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<header class="navbar" id="top" role="banner">
+					<div class="navbar-header">
+						<button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<div class="navbar-brand nav" id="brand">
+							<a href="index-google-map-fullscreen.html"><img src="assets/img/logo.png" alt="brand"></a>
+						</div>
+					</div>
+					<nav class="collapse navbar-collapse bs-navbar-collapse navbar-right" role="navigation">
+						<ul class="nav navbar-nav">
+							<li class="active has-child"><a href="#">Homepage</a>
+								<ul class="child-navigation">
+									<li><a href="index-google-map-fullscreen.html">Google Map Full Screen</a></li>
+									<li><a href="index-google-map-fixed-height.html">Google Map Fixed Height</a></li>
+									<li><a href="index-google-map-fixed-navigation.html">Google Map Fixed Navigation</a></li>
+									<li><a href="index-osm.html">OpenStreetMap Full Screen</a></li>
+									<li><a href="index-osm-fixed-height.html">OpenStreetMap Fixed Height</a></li>
+									<li><a href="index-osm-fixed-navigation.html">OpenStreetMap Fixed Navigation</a></li>
+									<li><a href="index-slider.html">Slider Homepage</a></li>
+									<li><a href="index-slider-search-box.html">Slider with Search Box</a></li>
+									<li><a href="index-horizontal-search-floated.html">Horizontal Search Floated</a></li>
+									<li><a href="index-advanced-horizontal-search.html">Horizontal Advanced Search</a></li>
+									<li><a href="index-slider-horizontal-search-box.html">Horizontal Slider Search</a></li>
+									<li><a href="index-slider-horizontal-search-box-floated.html">Horizontal Slider Floated Search</a></li>
+								</ul>
+							</li>
+							<li><a href="about.html">About</a></li>
+							<li><a href="contact.html">Contact</a></li>
+						</ul>
+					</nav><!-- /.navbar collapse-->
+					<div class="add-your-property">
+						<a href="submit.html" class="btn btn-default"><i class="fa fa-plus"></i><span class="text">Add Your Property</span></a>
+					</div>
+				</header><!-- /.navbar -->
+			</div><!-- /.container -->
+		</div><!-- /.navigation -->
+
+		<div class="container">
+			<div class="geo-location-wrapper">
+				<span class="btn geo-location"><i class="fa fa-map-marker"></i><span class="text">Find My Position</span></span>
+			</div>
+		</div>
+
+		<!-- Map -->
+		<div id="map" class="has-parallax"></div>
+		<!-- end Map -->
+
+		<!-- Search Box -->
+		<div class="search-box-wrapper">
+			<div class="search-box-inner">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-3 col-sm-4">
+							<div class="search-box map">
+								<form role="form" id="form-map" class="form-map form-search">
+									<h2>What do <b>you</b> want?</h2>
+									<div class="form-group">
+										<input type="text" class="form-control" id="search-box-property-id" placeholder="Property ID">
+									</div>
+									<div class="form-group">
+										<select name="type">
+											<option value="">Status</option>
+											<option value="1">Rent</option>
+											<option value="2">Sale</option>
+										</select>
+									</div><!-- /.form-group -->
+									<div class="form-group">
+										<select name="country">
+											<option value="">Country</option>
+											<option value="1">France</option>
+											<option value="2">Great Britain</option>
+											<option value="3">Spain</option>
+											<option value="4">Russia</option>
+											<option value="5">United States</option>
+										</select>
+									</div><!-- /.form-group -->
+									<div class="form-group">
+										<select name="city">
+											<option value="">City</option>
+											<option value="1">New York</option>
+											<option value="2">Los Angeles</option>
+											<option value="3">Chicago</option>
+											<option value="4">Houston</option>
+											<option value="5">Philadelphia</option>
+										</select>
+									</div><!-- /.form-group -->
+									<div class="form-group">
+										<select name="district">
+											<option value="">District</option>
+											<option value="1">Manhattan</option>
+											<option value="2">The Bronx</option>
+											<option value="3">Brooklyn</option>
+											<option value="4">Queens</option>
+											<option value="5">Staten Island</option>
+										</select>
+									</div><!-- /.form-group -->
+									<div class="form-group">
+										<select name="property-type">
+											<option value="">Property Type</option>
+											<option value="1">Apartment</option>
+											<option value="2">Condominium</option>
+											<option value="3">Cottage</option>
+											<option value="4">Flat</option>
+											<option value="5">House</option>
+										</select>
+									</div><!-- /.form-group -->
+									<div class="form-group">
+										<div class="price-range">
+											<input id="price-input" type="text" name="price" value="1000;299000">
+										</div>
+									</div>
+									<div class="form-group">
+										<button type="submit" class="btn btn-default">Search Now</button>
+									</div><!-- /.form-group -->
+								</form><!-- /#form-map -->
+							</div><!-- /.search-box.map -->
+						</div><!-- /.col-md-3 -->
+					</div><!-- /.row -->
+				</div><!-- /.container -->
+			</div><!-- /.search-box-inner -->
+		</div>
+		<!-- end Search Box -->
+
+		<!-- Page Content -->
+		<div id="page-content">
+			@yield('content')
+		</div>
+		<!-- end Page Content -->
+		<!-- Page Footer -->
+		<footer id="page-footer">
+			<div class="inner">
+				<section id="footer-main">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-3 col-sm-3">
+								<article>
+									<h3>About Us</h3>
+									<p>Vel fermentum ipsum. Suspendisse quis molestie odio. Interdum et malesuada fames ac ante ipsum
+										primis in faucibus. Quisque aliquet a metus in aliquet. Praesent ut turpis posuere, commodo odio
+										id, ornare tortor
+									</p>
+									<hr>
+									<a href="#" class="link-arrow">Read More</a>
+								</article>
+							</div><!-- /.col-sm-3 -->
+							<div class="col-md-3 col-sm-3">
+								<article>
+									<h3>Recent Properties</h3>
+									<div class="property small">
+										<a href="property-detail.html">
+											<div class="property-image">
+												<img alt="" src="assets/img/properties/property-06.jpg">
+											</div>
+										</a>
+										<div class="info">
+											<a href="property-detail.html"><h4>2186 Rinehart Road</h4></a>
+											<figure>Doral, FL 33178 </figure>
+											<div class="tag price">$ 72,000</div>
+										</div>
+									</div><!-- /.property -->
+									<div class="property small">
+										<a href="property-detail.html">
+											<div class="property-image">
+												<img alt="" src="assets/img/properties/property-09.jpg">
+											</div>
+										</a>
+										<div class="info">
+											<a href="property-detail.html"><h4>2479 Murphy Court</h4></a>
+											<figure>Minneapolis, MN 55402</figure>
+											<div class="tag price">$ 36,000</div>
+										</div>
+									</div><!-- /.property -->
+								</article>
+							</div><!-- /.col-sm-3 -->
+							<div class="col-md-3 col-sm-3">
+								<article>
+									<h3>Contact</h3>
+									<address>
+										<strong>Your Company</strong><br>
+										4877 Spruce Drive<br>
+										West Newton, PA 15089
+									</address>
+									+1 (734) 123-4567<br>
+									<a href="#">hello@example.com</a>
+								</article>
+							</div><!-- /.col-sm-3 -->
+							<div class="col-md-3 col-sm-3">
+								<article>
+									<h3>Useful Links</h3>
+									<ul class="list-unstyled list-links">
+										<li><a href="#">All Properties</a></li>
+										<li><a href="#">Privacy Policy</a></li>
+										<li><a href="#">Login and Register Account</a></li>
+										<li><a href="#">FAQ</a></li>
+										<li><a href="#">Terms and Conditions</a></li>
+									</ul>
+								</article>
+							</div><!-- /.col-sm-3 -->
+						</div><!-- /.row -->
+					</div><!-- /.container -->
+				</section><!-- /#footer-main -->
+				<section id="footer-thumbnails" class="footer-thumbnails"></section><!-- /#footer-thumbnails -->
+				<section id="footer-copyright">
+					<div class="container">
+						<span>Copyright © 2013. All Rights Reserved.</span>
+						<span class="pull-right"><a href="#page-top" class="roll">Go to top</a></span>
+					</div>
+				</section>
+			</div><!-- /.inner -->
+		</footer>
+		<!-- end Page Footer -->
+	</div>
+
+	<div id="overlay"></div>
+
+	<script type="text/javascript" src="assets/js/jquery-2.1.0.min.js"></script>
+	<script type="text/javascript" src="assets/js/leaflet.js"></script>
+	<script type="text/javascript" src="assets/js/leaflet.markercluster.js"></script>
+	<script type="text/javascript" src="assets/js/jquery-migrate-1.2.1.min.js"></script>
+	<script type="text/javascript" src="assets/bootstrap/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="assets/js/smoothscroll.js"></script>
+	<script type="text/javascript" src="assets/js/owl.carousel.min.js"></script>
+	<script type="text/javascript" src="assets/js/bootstrap-select.min.js"></script>
+	<script type="text/javascript" src="assets/js/jquery.validate.min.js"></script>
+	<script type="text/javascript" src="assets/js/jquery.placeholder.js"></script>
+	<script type="text/javascript" src="assets/js/icheck.min.js"></script>
+	<script type="text/javascript" src="assets/js/jquery.vanillabox-0.1.5.min.js"></script>
+	<script type="text/javascript" src="assets/js/retina-1.1.0.min.js"></script>
+	<script type="text/javascript" src="assets/js/jshashtable-2.1_src.js"></script>
+	<script type="text/javascript" src="assets/js/jquery.numberformatter-1.2.3.js"></script>
+	<script type="text/javascript" src="assets/js/tmpl.js"></script>
+	<script type="text/javascript" src="assets/js/jquery.dependClass-0.1.js"></script>
+	<script type="text/javascript" src="assets/js/draggable-0.1.js"></script>
+	<script type="text/javascript" src="assets/js/jquery.slider.js"></script>
+	<script type="text/javascript" src="assets/js/custom-map.js"></script>
+	<script type="text/javascript" src="assets/js/custom.js"></script>
+<!--[if gt IE 8]>
+<script type="text/javascript" src="assets/js/ie.js"></script>
 <![endif]-->
-
-<!-- Add your site or application content here -->
-<p>Hello world! This is HTML5 Boilerplate.</p>
-
-@yield('content')
-
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="js/plugins.js"></script>
-<script src="js/main.js"></script>
-
+<script>
+	_latitude = 48.871;
+	_longitude = 2.3;
+	createHomepageOSM(_latitude,_longitude);
+</script>
 </body>
 </html>
