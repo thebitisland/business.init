002706347e26cc359decd110573b8988715a9949
kadaki01 <juanljr2@gmail.com>
2014-11-01 12:08:50 -0700

    Twitter API working

--- app/routes.php
+++ app/routes.php
@@ -7,3 +7,9 @@ Route::get('/', function()
 
 Route::get('/twitterapi/{latitude}/{longitude}/{radius}', 'TwitterApiController@getTweets');
 Route::get('/idealistaapi/{latitude}/{longitude}/{radius}', 'TwitterApiController@getIdealista');
+
+Route::get('/getUsers', function()
+    {
+        return Twitter::getUsers(array('screen_name' => 'koke0117', 'format' => 'json'));
+    });
+
