e2979cb26c5f74cce85bde479817acacf1b71e37
kadaki01 <juanljr2@gmail.com>
2014-11-04 04:21:16 -0800

    Added about

--- app/routes.php
+++ app/routes.php
@@ -5,6 +5,11 @@ Route::get('/', function()
     return View::make('hello');
 });
 
+Route::get('about', function()
+    {
+        return View::make('about');
+    });
+
 Route::get('/twitterapi/{latitude}/{longitude}/{radius}', 'TwitterApiController@getTweets');
 Route::get('/idealistaapi/{latitude}/{longitude}/{radius}', 'TwitterApiController@getIdealista');
 
---
+++ app/views/about.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="container">
+
+<br/>
+    <br/>
+<h2 class="section-title">What is Business.init()?</h2>
+<p>The bit island team developed the Business.init() project for Madrid’s first open Datathon, MADdata, with the goal of creating a new, innovative approach to the way entrepreneurs obtain the information necessary to start a new business in Spain’s capital,  Madrid. 
+</p><p>We process enormous amounts of data from many different sources to give the users the information they need to know: where and how to create this new startup, based on business type, location, population, income, nearby transport and ease of access, public opinion on the area, related businesses nearby among others.</p>
+<br/>
+<h2 class="section-title">Under what circumstances was Business.init() created?</h2>
+<p>The idea behind Business.init() came to us during MADdata’s Datathon in Madrid. During the course of this event, teams were presented with several datasets and APIs regarding various aspects and elements of Madrid to query from different sources, ranging from the city hall, to the transport association to private business and professionals offering their private datasets to the open public.
+</p>
+<p>Under the massive amount of information offered, we wanted to see how we could use as much as the information as possible towards a single goal, instead of focusing on a single set of data. It was then when we saw a unique opportunity to use all of this data, all of this information to help professionals both incumbent and newcomers, to find the perfect place for their new businesses.
+</p>
+<br/>
+<h2 class="section-title">How does Business.init() work for you?</h2>
+<p>Simple, you will be presented with a map. The map. The map is your main input to the application and from there, you can immediately see the two overlays that separate Madrid into districts according to both population and income. You can also filter these overlays by means of the checkboxes that filter these two overlays by gender and nationality, useful for determining a target audience.
+</p><p>Once the area has been selected, you will have to check where in the area you would like to set up so after selecting what business type you want to open, we will show you not only incumbent firms and businesses, but also other interesting markers such as public transport for easy identification of public affluence. Now just select the circle tool and give us the specific area where you would like to start and well take it from there!
+</p><p>You have your district information, business type, competition and area, what else is there to do? Get the establishment! As soon as you chose your area, the most relevant establishment results for purchase and rent will be presented to you, together with a quick reference of the competition nearby.
+</p><p>Finally, you don’t really know an area until you know the people in it, that’s why a selection of related tweets is shown.
+</p>
+<br/>
+<h2 class="section-title">No, I mean, economically</h2>
+<p>We run on a very simple and rather innovative way of dealing with this ugly issue. You are opening a new business. That takes money, usually a lot, and having to pay money for every little service eventually adds up. This is when we came up with the following:
+</p><p>Initially we won’t charge anything. Really, not a thing. You have a lot to keep in mind as of now and we definitely don’t want to get in the way. The only thing we ask is 4% of your profit from your second year in business and only if you make more than $10,000 in benefits yearly.
+</p><p>This way we make sure we don’t interfere with your business and that you pay when and if you can afford it.
+</p>
+<br/>
+<br/>
+</div>
+
+@stop
+
